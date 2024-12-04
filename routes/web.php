6b9f90d26810cc5ce235aef60f710ae021d89ed3
Sergio <sergioramos@ieselrincon.es>
2024-12-04 14:41:15 +0000

    Instalamos Jetstream con Livewire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Redirigimos la página principal a la de login
+ */
+Route::get('/', function () {
+	return redirect()->route('login');
+});
+
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
